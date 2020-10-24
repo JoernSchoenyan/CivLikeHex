ee39aa2f355d267500a560778abc0e2614029c7c
JoernSchoenyan <joern.schoenyan@web.de>
2020-10-24 15:18:10 +0200

    Just some colored hex fields for now.

---
+++ Assets/Scripts/HexMesh.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+	Mesh hexMesh;
+	List<Vector3> vertices;
+	List<int> triangles;
+	List<Color> colors;
+
+	MeshCollider meshCollider;
+
+	private void Awake()
+	{
+		GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+		meshCollider = gameObject.AddComponent<MeshCollider>();
+		hexMesh.name = "Hex Mesh";
+		vertices = new List<Vector3>();
+		triangles = new List<int>();
+		colors = new List<Color>();
+	}
+
+    public void Triangulate(HexCell[] cells)
+    {
+		hexMesh.Clear();
+		vertices.Clear();
+		triangles.Clear();
+		colors.Clear();
+
+		for (int i = 0; i < cells.Length; i++)
+		{
+			Triangulate(cells[i]);
+		}
+
+		hexMesh.vertices = vertices.ToArray();
+		hexMesh.triangles = triangles.ToArray();
+		hexMesh.colors = colors.ToArray();
+		hexMesh.RecalculateNormals();
+		meshCollider.sharedMesh = hexMesh;
+	}
+
+	private void Triangulate(HexCell cell)
+	{
+        for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
+        {
+			Triangulate(d, cell);
+        }
+	}
+
+    private void Triangulate(HexDirection direction, HexCell cell)
+    {
+		Vector3 center = cell.transform.localPosition;
+		Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+		Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+		AddTriangle(center, v1, v2);
+		AddTriangleColor(cell.color);
+
+		if (direction <= HexDirection.SE)
+        {
+			TriangulateConnection(direction, cell, v1, v2);
+		}
+	}
+
+	private void TriangulateConnection(HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2)
+    {
+		HexCell neighbor = cell.GetNeighbor(direction);
+		if (neighbor == null)
+        {
+			return;
+        }
+
+		Vector3 bridge = HexMetrics.GetBridge(direction);
+		Vector3 v3 = v1 + bridge;
+		Vector3 v4 = v2 + bridge;
+
+		AddQuad(v1, v2, v3, v4);
+		AddQuadColor(cell.color, neighbor.color);
+
+		HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+		if (direction <= HexDirection.E && nextNeighbor != null)
+        {
+			AddTriangle(v2, v4, v2 + HexMetrics.GetBridge(direction.Next()));
+			AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
+        }
+	}
+
+    private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+	{
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+	}
+
+	private void AddTriangleColor(Color color)
+	{
+		colors.Add(color);
+		colors.Add(color);
+		colors.Add(color);
+	}
+
+	private void AddTriangleColor(Color c1, Color c2, Color c3)
+    {
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c3);
+	}
+
+	private void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
+    {
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		vertices.Add(v4);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 2);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+		triangles.Add(vertexIndex + 3);
+	}
+
+	private void AddQuadColor(Color c1, Color c2, Color c3, Color c4)
+	{
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c3);
+		colors.Add(c4);
+	}
+
+	private void AddQuadColor(Color c1, Color c2)
+    {
+		colors.Add(c1);
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c2);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CivLikeHex
-Hex maps in Unity3D similar to Civ5. Based on the tutorial series from catlikecoding.com
+Hex maps in Unity3D similar to Civ5. Based on the tutorial series from catlikecoding.com, but using the Universal Render Pipeline (URP) and Shader Graph.
