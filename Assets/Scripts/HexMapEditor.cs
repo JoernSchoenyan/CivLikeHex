2ffc29d0f2b407105d26d02d1c8f4c20e130b574
JoernSchoenyan <joern.schoenyan@web.de>
2020-10-24 21:03:38 +0200

    simple map editor

---
+++ Assets/Scripts/HexMapEditor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexMapEditor : MonoBehaviour
+{
+
+	public Color[] colors;
+
+	public HexGrid hexGrid;
+
+	private Color activeColor;
+
+	private void Awake()
+	{
+		SelectColor(3);
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+		{
+			HandleInput();
+		}
+	}
+
+	public void SelectColor(int index)
+	{
+		activeColor = colors[index];
+	}
+
+	private void HandleInput()
+	{
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(inputRay, out hit))
+		{
+			EditCell(hexGrid.GetCell(hit.point));
+		}
+	}
+
+    private void EditCell(HexCell cell)
+    {
+		cell.color = activeColor;
+		hexGrid.Refresh();
+    }
+}
